cd2bf8bc6e1e14448bd05bfe82239b554c83670b
Melinh Mai <melinh.mai@gmail.com>
2018-05-21 19:27:40 -0500

    correction

--- game.js
+++ game.js
@@ -1,120 +1,124 @@
-// A game that starts with a given number to to get closest to
-// you have 4 crystal to pick from, each crystal has random numbers 
-// to pick from and you are trying not to go over on the random numbers
 
-// calling out the variables and selecting a random number to be shown in Number to Get
+$(document).ready(function(){
+  // A game that starts with a given number to to get closest to
+  // you have 4 crystal to pick from, each crystal has random numbers 
+  // to pick from and you are trying not to go over on the random numbers
 
-
-var yourScore = 0;
-var numLosses = 0;
-var userTotal = 0;
-var losses = 0;
-wins = 0;
+  // calling out the variables and selecting a random number to be shown in Number to Get
 
 
-var compPicker =Math.floor(Math.random()*120+19);
+  var yourScore = 0;
+  var numLosses = 0;
+  var userTotal = 0;
+  var losses = 0;
+  wins = 0;
+
+
+  var compPicker =Math.floor(Math.random()*120+19);
 
 
 
 
 
-// gernerating random number function to the id Gem1-4
-// when click on the Gem, the value will change in html
+  // gernerating random number function to the id Gem1-4
+  // when click on the Gem, the value will change in html
 
-var Gem1 = Math.floor(Math.random()*12+1);
-var Gem2 = Math.floor(Math.random()*12+1);
-var Gem3 = Math.floor(Math.random()*12+1);
-var Gem4 = Math.floor(Math.random()*12+1);
+  var Gem1 = Math.floor(Math.random()*12+1);
+  var Gem2 = Math.floor(Math.random()*12+1);
+  var Gem3 = Math.floor(Math.random()*12+1);
+  var Gem4 = Math.floor(Math.random()*12+1);
 
-// declaring variables for wins and loss
+  // declaring variables for wins and loss
 
-$('#numWins').text(yourScore);
-$('#numLosses').text(numLosses);
+  $('#numWins').text(yourScore);
+  $('#numLosses').text(numLosses);
 
-//start the game
-function start(){
-      numRand=Math.floor(Math.random()*120+19);
-      compPicker =Math.floor(Math.random()*120+19);
-      console.log(compPicker)
-      $('#randomNumber').text(compPicker);
-      Gem1 = Math.floor(Math.random()*12+1);
-      Gem2 = Math.floor(Math.random()*12+1);
-      Gem3 = Math.floor(Math.random()*12+1);
-      Gem4 = Math.floor(Math.random()*12+1);
-      userTotal = 0; 
-      $('#finalTotal').text(userTotal);
-    }
-    
+  //start the game
+  function start(){
+        numRand=Math.floor(Math.random()*120+19);
+        compPicker =Math.floor(Math.random()*120+19);
+        console.log(compPicker)
+        $('#randomNumber').text(compPicker);
+        Gem1 = Math.floor(Math.random()*12+1);
+        Gem2 = Math.floor(Math.random()*12+1);
+        Gem3 = Math.floor(Math.random()*12+1);
+        Gem4 = Math.floor(Math.random()*12+1);
+        userTotal = 0; 
+        $('#finalTotal').text(userTotal);
+      }
+      
 
-    start();
-
-    //adds wins to userTotal
-    function win(){
-
-      alert("You win!");
-      wins++;
-      $("#numWins").text(yourScore);
       start();
-    }
 
-    // add losses to userTotal
-    function lose(){
-      alert ("You Lose!");
-        losses++;
-        $('#numLosses').text(losses);
+      //adds wins to userTotal
+      function win(){
+
+        alert("You win!");
+        wins++;
+        $("#numWins").text(yourScore);
         start();
-    }
+      }
 
-    // when clicking each Gem, a number will generate
-    // user will click another Gem to get another random number that will be added to the 1st click
-    // user will continue to click another Gem to get the target number that was given 
+      // add losses to userTotal
+      function lose(){
+        alert ("You Lose!");
+          losses++;
+          $('#numLosses').text(losses);
+          start();
+      }
 
-    $('#Gem1').on('click', function(){
-      userTotal = userTotal + Gem1;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal);
-        if (userTotal == compPicker){
-          win();
-        }
-        else if ( userTotal > compPicker){
-          lose();
-        }
-    })
+      // when clicking each Gem, a number will generate
+      // user will click another Gem to get another random number that will be added to the 1st click
+      // user will continue to click another Gem to get the target number that was given 
 
-    $('#Gem2').on('click', function(){
-      userTotal = userTotal + Gem2;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal);
-        if (userTotal == compPicker){
-          win();
-        }
-        else if ( userTotal > compPicker){
-          lose();
-        }
-    })
+      $('#Gem1').on('click', function(){
+        userTotal = userTotal + Gem1;
+        console.log("New userTotal= " + userTotal);
+        $('#finalTotal').text(userTotal);
+          if (userTotal == compPicker){
+            win();
+          }
+          else if ( userTotal > compPicker){
+            lose();
+          }
+      })
 
-    $('#Gem3').on('click', function(){
-      userTotal = userTotal + Gem3;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal);
+      $('#Gem2').on('click', function(){
+        userTotal = userTotal + Gem2;
+        console.log("New userTotal= " + userTotal);
+        $('#finalTotal').text(userTotal);
+          if (userTotal == compPicker){
+            win();
+          }
+          else if ( userTotal > compPicker){
+            lose();
+          }
+      })
 
-// win/lose conditions
-    if (userTotal == compPicker){
-       win();
-    }
-    else if ( userTotal > compPicker){
-      lose();
-}
-})
+      $('#Gem3').on('click', function(){
+        userTotal = userTotal + Gem3;
+        console.log("New userTotal= " + userTotal);
+        $('#finalTotal').text(userTotal);
 
-$('#Gem4').on('click', function(){
-  userTotal = userTotal + Gem4;
-  console.log("New userTotal= " + userTotal);
-  $('#finalTotal').text(userTotal);
-    if (userTotal == randomNumber){
-      win();
-    }
-    else if ( userTotal > compPicker){
-      lose();
-    }
-})
+  // win/lose conditions
+      if (userTotal == compPicker){
+        win();
+      }
+      else if ( userTotal > compPicker){
+        lose();
+  }
+  })
+
+  $('#Gem4').on('click', function(){
+    userTotal = userTotal + Gem4;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal);
+      if (userTotal == randomNumber){
+        win();
+      }
+      else if ( userTotal > compPicker){
+        lose();
+      }
+  });
+
+});
